48eff6cc46af94c20618abfe62251f61e1e2a20a
Luke Sheppard <sheppiegit@gmail.com>
2023-05-08 11:14:11 -0400

    constants

---
+++ Constants.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity ^0.8.17;
+
+contract Constants {
+    //coding convention to uppercase constant variables
+    address public constant MY_ADDRESS =
+        0x777788889999AaAAbBbbCcccddDdeeeEfFFfCcCc;
+    uint public constant MY_UINT = 123;
+}
